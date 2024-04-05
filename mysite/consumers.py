4251b548e18082cb6cfcbfe83f6beb209171e945
Mari2405 <102598796+Mari2405@users.noreply.github.com>
2024-04-05 18:13:26 +0200

    Connessione-primo tentativo stream

--- mysite/consumers.py
+++ mysite/consumers.py
@@ -16,15 +16,18 @@ class VideoStreamConsumer(AsyncWebsocketConsumer):
     async def stream_video(self):
         # Inizializza la cattura video
         cap = cv2.VideoCapture(0)
-        while True:
-            ret, frame = cap.read()
-            if not ret:
-                break
-            # Converte il frame in JPEG
-            ret, jpeg = cv2.imencode('.jpg', frame)
-            # Invia i dati convertiti in byte
-            await self.send(bytes_data=jpeg.tobytes())
-            # Aspetta prima di inviare il prossimo frame (regolare in base al frame rate)
-            await asyncio.sleep(0) 
+        try:
+            while True:
+                ret, frame = cap.read()
+                if not ret:
+                    print("Impossibile leggere il frame")
+                # Converte il frame in JPEG
+                ret, jpeg = cv2.imencode('.jpg', frame, [int(cv2.IMWRITE_JPEG_QUALITY), 70]) #riduce la qualità del frame per migliorare la trasmissione dei dati
+                # Invia i dati convertiti in byte
+                await self.send(bytes_data=jpeg.tobytes())
+                # Aspetta prima di inviare il prossimo frame (regolare in base al frame rate)
+                await asyncio.sleep(0) 
+        except Exception as e: 
+            print(f"Errore durante la trasmissione del video: {e}")
         # Rilascia la cattura alla fine del loop
         cap.release()
