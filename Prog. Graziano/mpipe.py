8604ebfce30cffcc4e72d76a45844a8a3aa43846
Mari2405 <102598796+Mari2405@users.noreply.github.com>
2024-04-12 16:07:55 +0200

    Fixed - Stop stream

--- Prog. Graziano/mpipe.py
+++ Prog. Graziano/mpipe.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, render_template, Response, request, redirect, url_for, session
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired
@@ -9,15 +8,10 @@ import os
 import mediapipe as mp
 
 # Initialize MediaPipe Hands
-mp_hands = mp.solutions.hands
-hands = mp_hands.Hands()
-
-import mediapipe as mp
-
-# Initialize MediaPipe Hands
-mp_hands = mp.solutions.hands
-hands = mp_hands.Hands()
 mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+pose = mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) 
+
 
 # Setting
 app = Flask(__name__)
@@ -36,19 +30,22 @@ def detect_sign_language(frame):
     # Convert BGR to RGB
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     # Process the frame
-    with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
-        process_frames = hands.process(rgb_frame)
+    with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+        process_frames = pose.process(rgb_frame)
         # Draw landmarks on the frame
-        if process_frames.multi_hand_landmarks:
-            for lm in process_frames.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(frame, lm, mp_hands.HAND_CONNECTIONS)
+        if process_frames.pose_landmarks:
+                mp_drawing.draw_landmarks(
+                    frame, 
+                    process_frames.pose_landmarks, 
+                    mp_pose.POSE_CONNECTIONS,
+                    mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2),
+                    mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
+                )
     return frame
-# Release resources
-hands.close()
-
 
 # Function to generate video frames
 def generate_frames():
+    camera = cv2.VideoCapture(0)
     while True:
         success, frame = camera.read()
         if not success:
@@ -60,20 +57,19 @@ def generate_frames():
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
+
+# Release resources
+pose.close()
+
 # Route for the video feed
 @app.route('/video_feed')
 def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
+# Route for show video
 @app.route('/video_input')
 def video_input():
-    # Questa route ora renderizza un template HTML
     return render_template('video_stream.html')
-
-# Route for the main page
-@app.route('/')
-def index():
-    return render_template('home.html')
 
 # Route for video upload
 @app.route('/video_upload', methods=['GET', 'POST'])
@@ -89,14 +85,19 @@ def video_upload():
 
         # Process video
         ### 
-        # Get vide url
+
+        # Get video url
         video_url = url_for('static', filename= f'videos/{filename}')
         return render_template('view_video.html', video_url=video_url)
         # return redirect(url_for('view_video'))
     # Redirect user
     return render_template('video_upload.html', form=form)
 
-
+# Route for the main page
+@app.route('/')
+def index():
+    camera.release()
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
